724c5f7cd445caae0c1feb480916a8acefc4347a
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-08-29 10:26:21 +0300

    mergeSort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,37 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if(nums.length < 2) {
+    return nums
+  }
+
+  const middle = Math.floor(nums.length / 2)
+
+  const left = nums.slice(0, middle)
+  const right = nums.slice(middle)
+
+  const sortLeft = mergeSort(left)
+  const sortRight = mergeSort(right)
+  
+  return merge(sortLeft, sortRight) 
 };
 
+function merge(a,b){
+  const result = []
+
+  while(a.length > 0 && b.length > 0) {
+    if(a[0] < b[0]) {
+      result.push(a.shift())
+    }else {
+      result.push(b.shift())
+    }
+  }
+
+  return result.concat(a, b);
+}
+
 // unit tests
-// do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
